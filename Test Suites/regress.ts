ec1ea146d3ffbdd1fc388f467f51cca537b50c71
oleg <oleg@polaz.com>
2018-05-21 15:31:45 +0300

    1

--- Test Suites/regress.ts
+++ Test Suites/regress.ts
@@ -4,10 +4,17 @@
    <name>regress</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-05-21T14:59:20</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>d9e69f15-e4bf-47b9-a40c-044356a86b33</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1834161-b3a1-4556-806d-396b2e9f134f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login with invalid data</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
