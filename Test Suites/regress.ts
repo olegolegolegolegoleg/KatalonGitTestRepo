831fb21dff4cf21b2b2544802723b05ad800c3a9
oleg <oleg@polaz.com>
2018-05-21 12:53:26 +0300

    add new test suite

---
+++ Test Suites/regress.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>regress</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d9e69f15-e4bf-47b9-a40c-044356a86b33</testSuiteGuid>
+</TestSuiteEntity>
