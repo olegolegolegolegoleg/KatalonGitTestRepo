def270f08313d255db3a00f4d649cbc533f15ac2
oleg <oleg@polaz.com>
2018-06-04 11:58:53 +0300

    add new2

---
+++ Test Suites/mobile-2-authorization.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>mobile-2-authorization</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-01T11:40:20</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e75027a9-1d69-482f-921e-806b46381f00</testSuiteGuid>
+   <testCaseLink>
+      <guid>1f119581-7bc6-4809-9a17-a8465ef592d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-12_Login_valid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6319edb-0855-4cbe-895f-c9d7f5f0996d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-16_Login_invalid</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/mobile-3-balances.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>mobile-3-balances</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-04T11:02:07</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b77b7224-46dd-4f33-bfb4-dab10ba3c344</testSuiteGuid>
+   <testCaseLink>
+      <guid>538a3469-e48f-4c33-b990-5c72db54fb46</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-41_Currency exchange - buy_Valid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96c91d0c-7a80-4fcf-a3b1-148394128fa6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-42_Currency exchange - buy_invalid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3caf93b4-38bd-447b-86c5-444b212cf124</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-43_Currency exchange - sell_valid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1011fa0e-fa94-4ed1-b649-98f2e65ae598</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-44_Currency exchange - sell_invalid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4b4d2915-8e17-47b0-a729-3d399a64fbd5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-15_Euro-USD_deposit _valid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dac1fa36-5b04-4b49-beb9-418e2f38e976</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile_BT-17_Euro-USD_deposit_invalid</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
